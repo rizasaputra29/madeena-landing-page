3efd532b3b227f62d7e857f3330a59b6542e3d1e
Riza Saputra <riza@192.168.40.190>
2026-01-14 19:01:48 +0700

    fix: login error

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_APP_URL ?? 'https://almadeenaislamicschool.sch.id/';
+  const baseUrl = process.env.NEXT_PUBLIC_APP_URL ?? 'https://almadeenaislamicschool.sch.id';
 
   return {
     rules: {
